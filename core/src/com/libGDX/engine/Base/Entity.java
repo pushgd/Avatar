fa9056e1a234ddd9a0924f5931644755bd69a494
Pushgd <newtondhande@gmail.com>
2017-02-27 23:52:06 +0530

    Added Debug package Added scene and entity

--- core/src/com/libGDX/engine/Base/Entity.java
+++ core/src/com/libGDX/engine/Base/Entity.java
@@ -1,14 +1,33 @@
 package com.libGDX.engine.Base;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by Dhande on 27-02-2017.
  */
 
-public class Entity
+public abstract class Entity
 {
 
+    private int baseId = 0;
 
-    int ID;
+    public int ID;
+    private int UID;
+    public Point position;
+    public Point velocity;
 
+    public Entity()
+    {
+        UID = baseId++;
+    }
+
+    public abstract void update();
+
+    public abstract void paint(SpriteBatch spriteBatch);
+
+    public int getUID()
+    {
+        return UID;
+    }
 
 }
---
+++ core/src/com/libGDX/engine/Base/GameObject.java
@@ -0,0 +1,13 @@
+package com.libGDX.engine.Base;
+
+/**
+ * Created by Dhande on 27-02-2017.
+ */
+
+public abstract class GameObject extends Entity
+{
+
+
+
+
+}
